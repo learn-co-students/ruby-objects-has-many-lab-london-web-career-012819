f49c4f3738b61293c24bbc0be1cc605210e8cee1
Alexander Dubois <sander.dubois@outlook.com>
2019-02-01 09:39:50 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+require "pry"
+class Artist
+
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    song.artist = self
+    songs << song
+  end
+
+  def add_song_by_name(song_name)
+    new_song = Song.new(song_name)
+    add_song(new_song)
+
+    new_song
+  end
+
+  def artist_name
+
+  end
+
+  def self.song_count
+    Song.all.length
+  end
+
+end
