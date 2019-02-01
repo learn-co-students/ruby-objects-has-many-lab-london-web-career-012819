f5c9d0dc8e5bdef051e68751e129cb5e7f5abe66
SofiaFredriksson <sofiapkf@gmail.com>
2019-02-01 11:33:10 +0000

    Done

---
+++ lib/author.rb
@@ -0,0 +1,28 @@
+class Author
+attr_reader :name
+
+def initialize(name)
+  @name = name
+  @posts = []
+end
+
+def posts
+  @posts
+end
+
+def add_post(post)
+  post.author = self
+  @posts << post
+end
+
+def add_post_by_title(title)
+  post = Post.new(title)
+  @posts << post
+  post.author = self
+end
+
+def self.post_count
+  Post.all.length
+end
+
+end
